305b8476e766963c1b3e111d7a08bae3236e0fac
nguyenduchieu <hieunguyenduc1104@gmail.com>
2023-06-17 13:57:03 +0700

    feat: create race module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RacesModule } from './races/races.module';
 
 @Module({
-  imports: [],
+  imports: [RacesModule],
   controllers: [AppController],
   providers: [AppService],
 })
